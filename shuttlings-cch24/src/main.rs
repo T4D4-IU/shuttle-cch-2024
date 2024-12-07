498e911b83a6a1ecf466e4bae3d257e2c1b98d3e
T4D4 .icp <t4d4.icp@gmail.com>
2024-12-07 14:09:41 +0000

    change func name world->bird

--- shuttlings-cch24/src/main.rs
+++ shuttlings-cch24/src/main.rs
@@ -2,14 +2,14 @@ use actix_web::{get, web::ServiceConfig};
 use shuttle_actix_web::ShuttleActixWeb;
 
 #[get("/")]
-async fn hello_world() -> &'static str {
+async fn hello_bird() -> &'static str {
     "Hello, bird!"
 }
 
 #[shuttle_runtime::main]
 async fn main() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
     let config = move |cfg: &mut ServiceConfig| {
-        cfg.service(hello_world);
+        cfg.service(hello_bird);
     };
 
     Ok(config.into())
