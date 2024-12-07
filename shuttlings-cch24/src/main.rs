759213d9c410e526ea4c7f9340d8ebfb551cdb58
T4D4 .icp <t4d4.icp@gmail.com>
2024-12-07 14:36:38 +0000

    solve Day1-task2

--- shuttlings-cch24/src/main.rs
+++ shuttlings-cch24/src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{get, web::ServiceConfig};
+use actix_web::{get, http::header::LOCATION, web::ServiceConfig, HttpResponse};
 use shuttle_actix_web::ShuttleActixWeb;
 
 #[get("/")]
@@ -6,10 +6,18 @@ async fn hello_bird() -> &'static str {
     "Hello, bird!"
 }
 
+#[get("/{id}/seek")]
+async fn seek() -> HttpResponse {
+    HttpResponse::Found()
+        .insert_header((LOCATION, "https://www.youtube.com/watch?v=9Gc4QTqslN4"))
+        .finish()
+}
+
 #[shuttle_runtime::main]
 async fn main() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
     let config = move |cfg: &mut ServiceConfig| {
         cfg.service(hello_bird);
+        cfg.service(seek);
     };
 
     Ok(config.into())
